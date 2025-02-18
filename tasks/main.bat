f7a6246f4895ad4040e650711277d2982507f28f
Gobinda Prasad Jamakatel <gobindaprasadjamakatel@NP-D2VVH77WL7.local>
2025-02-18 13:09:31 +0545

    update: updates bat script

--- tasks/main.bat
+++ tasks/main.bat
@@ -1,3 +1,3 @@
-cd C:\Users\shiva.basnet\PycharmProjects\Cloud Subscription\
-call venv\scripts\activate.bat
+C:\Users\shiva.basnet\PycharmProjects\OpenStreetBackend
+call env\scripts\activate.bat
 python main.py
